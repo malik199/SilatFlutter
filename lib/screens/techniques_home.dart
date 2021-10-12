db0ad8350c2a74f94157f9b0b13a09d2fca9aa6c
malik199 <malik199@gmail.com>
2021-10-11 21:36:24 -0400

    Thumbnail and stripes working

--- lib/screens/techniques_home.dart
+++ lib/screens/techniques_home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'techniques_list.dart';
 
 class TechniquesHome extends StatefulWidget {
   const TechniquesHome({Key? key}) : super(key: key);
@@ -13,160 +15,204 @@ class _TechniquesHomeState extends State<TechniquesHome> {
   final textcontroller = TextEditingController();
   final _database = FirebaseDatabase.instance.reference();
   final Future<FirebaseApp> _future = Firebase.initializeApp();
+  User? _user = FirebaseAuth.instance.currentUser;
 
-  /*void addData(String data) {
-    databaseRef.push().set({'name': data, 'comment': 'A good season'});
-  }*/
+  @override
+  void initState() {
+    getYourTechniques();
+  }
+
   var myData;
-  void printFirebase(){
-    /*_database.once().then((DataSnapshot snapshot) {
-      print('Data : ${snapshot.value}');
-    });*/
-
-    _database.child('techniques/0').get().then((DataSnapshot snapshot) {
-        final data = new Map<String, dynamic>.from(snapshot.value);
-        print('New Data: ${data}');
-    });
-
-    /*
-    _database.child('techniques/').onValue.listen((event) {
-      final data = new Map<String, dynamic>.from(event.snapshot.value);
-      print('Data : ${description}');
-    });
-
-    _database.child('users/0/firstname').onValue.listen((event) {
-      final String description = event.snapshot.value;
-      print('Data : ${description}');
-    });
-     */
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    printFirebase();
-    return FutureBuilder(
-          future: _future,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Text(snapshot.error.toString());
-            } else {
-              return Container(
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 250.0),
-                    Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: TextField(
-                        controller: textcontroller,
-                      ),
-                    ),
-                    SizedBox(height: 30.0),
-                    Center(
-                        child: RaisedButton(
-                            color: Colors.pinkAccent,
-                            child: Text("Save to Database"),
-                            onPressed: () {
-                              //addData(textcontroller.text);
-                              //call method flutter upload
-                            }
-                        )
-                    ),
-                  ],
-                ),
-              );
-            }
-          }
-    );
-  }
-}
-
-
-
-/*
-import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
-
-class TechniquesHome extends StatefulWidget {
-  const TechniquesHome({Key? key}) : super(key: key);
-
-  @override
-  _TechniquesHomeState createState() => _TechniquesHomeState();
-}
-
-class _TechniquesHomeState extends State<TechniquesHome> {
-  bool _active = false;
-
-  void _handleTapboxChanged(bool newValue) {
-    setState(() {
-      _active = newValue;
+  var myList;
+  void getYourTechniques() {
+    _database
+        .child('users')
+        .orderByChild('email')
+        .equalTo((_user?.email)?.toLowerCase())
+        .limitToFirst(1)
+        .once()
+        .then((DataSnapshot snapshot) {
+      final data = new Map<String?, dynamic>.from(snapshot.value);
+      setState(() {
+        data.forEach((key, value) {
+          myList = value;
+          print('My List ${myList}');
+        });
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Container(
-        child: Column(
-          children: [
-            Icon(
-              Icons.sports_kabaddi,
-              size: 150,
-            ),
-            Text(
-              "This is the new technique section of the website!"
-            )
-          ],
+    const double _spacing = 18;
+
+    List _fullBeltArray = [
+      "white",
+      "yellow",
+      "green",
+      "blue",
+      "purple",
+      "brown",
+      "black",
+      "red"
+    ];
+
+    List allYourBelts() {
+      List myBeltLevels = [];
+      for (var i = 0; i <= _fullBeltArray.indexOf(myList?['belt']); i++) {
+        myBeltLevels.add(_fullBeltArray[i]);
+      }
+      return myBeltLevels;
+    }
+
+    print(allYourBelts());
+
+    Widget produceBelts(List<dynamic> _allbelts) {
+      return Column(
+          children: _allbelts
+              .map(
+                (item) => Belt(curriculum: myList['curriculum'], color: item),
+              )
+              .toList());
+    }
+
+    return Column(children: [
+      Padding(
+        padding: const EdgeInsets.all(_spacing),
+        child: Text(
+          ("${myList?['firstname']}'s techniques").toUpperCase(),
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
         ),
-      ) ,
-    );
+      ),
+      produceBelts(allYourBelts()),
+      SizedBox(height: _spacing),
+      ElevatedButton.icon(
+        label: Text('Admin Console'),
+        icon: Icon(Icons.settings),
+        onPressed: () {
+          print('Pressed');
+        },
+      )
+    ]);
   }
 }
 
+class Belt extends StatelessWidget {
+  final String curriculum;
+  final String color;
+  const Belt({
+    Key? key,
+    required this.curriculum,
+    required this.color,
+  }) : super(key: key);
 
+  final double _beltHeight = 50;
+  final double _innerPadding = 15;
+  final double _borderRadius = 4;
 
-@override
+  @override
   Widget build(BuildContext context) {
-    printFirebase();
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Firebase Demo"),
-      ),
-      body: FutureBuilder(
-          future: _future,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Text(snapshot.error.toString());
-            } else {
-              return Container(
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 250.0),
-                    Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: TextField(
-                        controller: textcontroller,
-                      ),
-                    ),
-                    SizedBox(height: 30.0),
-                    Center(
-                        child: RaisedButton(
-                            color: Colors.pinkAccent,
-                            child: Text("Save to Database"),
-                            onPressed: () {
-                              //addData(textcontroller.text);
-                              //call method flutter upload
-                            }
-                        )
-                    ),
-                  ],
-                ),
-              );
-            }
+    Color getColor(color) {
+      switch (color) {
+        case 'white':
+          {
+            return Colors.white;
           }
-      ),
-    );
+          break;
+
+        case 'yellow':
+          {
+            return Colors.yellow;
+          }
+          break;
+
+        case 'green':
+          {
+            return Colors.green;
+          }
+          break;
+
+        case 'blue':
+          {
+            return Colors.blue;
+          }
+          break;
+
+        case 'purple':
+          {
+            return Colors.purple;
+          }
+          break;
+
+        case 'brown':
+          {
+            return Colors.brown;
+          }
+          break;
+
+        case 'black':
+          {
+            return Colors.black;
+          }
+          break;
+
+        case 'red':
+          {
+            return Colors.red;
+          }
+          break;
+
+        default:
+          {
+            return Colors.white;
+          }
+          break;
+      }
+    }
+
+    if (curriculum == 'jawara_muda') {
+      return TextButton(
+        onPressed: () {
+          print("Container clicked");
+        },
+        child: Container(
+          height: _beltHeight,
+          decoration: BoxDecoration(
+            color: getColor(color),
+            border: Border.all(
+              color: Colors.grey,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(_borderRadius),
+          ),
+        ),
+      );
+    } else {
+      return TextButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => TechniquesList()),
+          );
+        },
+        child: Container(
+          height: _beltHeight,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.grey,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(_borderRadius),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: _innerPadding),
+            child: Container(
+              color: getColor(color),
+            ),
+          ),
+        ),
+      );
+    }
   }
-
-
-
-*/
+}
--- lib/screens/techniques_list.dart
+++ lib/screens/techniques_list.dart
@@ -1,301 +1,184 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_core/firebase_core.dart';
-import '../models/technique_list.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
-class TechniquesHome extends StatefulWidget {
-  const TechniquesHome({Key? key}) : super(key: key);
+//a7dba65b5ca93ee1c513b8926987343c Vimeo Private Key
+
+class TechniquesList extends StatefulWidget {
+  const TechniquesList({Key? key}) : super(key: key);
 
   @override
-  _TechniquesHomeState createState() => _TechniquesHomeState();
+  _TechniquesListState createState() => _TechniquesListState();
 }
 
-class _TechniquesHomeState extends State<TechniquesHome> {
-  final textcontroller = TextEditingController();
-  final _database = FirebaseDatabase.instance.reference();
-  final Future<FirebaseApp> _future = Firebase.initializeApp();
-  User? _user = FirebaseAuth.instance.currentUser;
-
-  /*void addData(String data) {
-    databaseRef.push().set({'name': data, 'comment': 'A good season'});
-  }*/
-
-  var myData;
-  var myList;
-  void getYourTechniques() {
-
-    _database.child('users').orderByChild('email').equalTo((_user?.email)?.toLowerCase()).limitToFirst(1).once().then((DataSnapshot snapshot) {
-      final data = new Map<String?, dynamic>.from(snapshot.value);
-      data.forEach((key, value) {
-        myList = value;
-      });
-      print(myList['firstname']);
-    });
+class _TechniquesListState extends State<TechniquesList> {
+  DatabaseReference reference =
+      FirebaseDatabase.instance.reference().child('techniques');
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    const double _beltWidth = 36;
-    const double _spacing = 18;
-    const double _stripeSpacing = 8;
+  final vimeoKey = "a7dba65b5ca93ee1c513b8926987343c";
+  //var imagepic;
+  Future fetchAlbum(videoId) async {
+    final response = await http.get(
+      Uri.parse('https://api.vimeo.com/videos/$videoId'),
+      // Send authorization headers to the backend.
+      headers: {
+        'Accept': 'application/vnd.vimeo.*+json;version=3.4',
+        'Authorization': 'Bearer $vimeoKey',
+      },
+    );
+    final responseJson = jsonDecode(response.body);
 
-    getYourTechniques();
-    var output = _database.child('techniques').orderByChild('jawara_muda').equalTo('yellow').once();
-    print(output);
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(_spacing),
-          child: Text(("${myList['firstname']}'s techniques").toUpperCase()),
-        ),
-        TextButton(
-          onPressed: (){
-            print("Container clicked");
-          },
+    //print(responseJson?["pictures"]["sizes"][0]["link"]);
+    return responseJson?["pictures"]["sizes"][2]["link"];
+    //return Album.fromJson(responseJson);
+  }
+
+  Widget _buildTechniqueItem({Map? dbItem, myIndex}) {
+    final double _beltHeight = 35;
+    final double _borderRadius = 4;
+    final double _stripeWidth = 10;
+    final double _stripeSpacing = 10;
+
+    Widget showHeader() {
+      if (dbItem?["jm_showstripe"] != "") {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Container(
-            height: _beltWidth,
-            decoration: BoxDecoration(
-              color: Colors.white,
+              height: _beltHeight,
+              decoration: BoxDecoration(
+                color: Colors.green,
                 border: Border.all(
-                color: Colors.grey,
-                width: 1,
-              ),
-              borderRadius: BorderRadius.circular(1),
-            ),
-          ),
-        ),
-        TextButton(
-          onPressed: (){
-            print("Container clicked");
-          },
-          child: Container(
-            height: 30,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(
-                color: Colors.grey,
-                width: 1,
-              ),
-              borderRadius: BorderRadius.circular(1),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: _stripeSpacing),
-              child: Container(
-                color: Colors.yellow,
-              ),
-            ),
-          ),
-        ),
-        SizedBox(height: _spacing),
-        Container(
-          height: 30,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(
-              color: Colors.grey,
-              width: 1,
-            ),
-            borderRadius: BorderRadius.circular(1),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: _stripeSpacing),
-            child: Container(
-              color: Colors.green,
-            ),
-          ),
-        ),
-        SizedBox(height: _spacing),
-        Container(
-          height: 30,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(
-              color: Colors.grey,
-              width: 1,
-            ),
-            borderRadius: BorderRadius.circular(1),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: _stripeSpacing),
-            child: Container(
-              color: Colors.blue,
-            ),
-          ),
-        ),
-        SizedBox(height: _spacing),
-        Container(
-          height: 30,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(
-              color: Colors.grey,
-              width: 1,
-            ),
-            borderRadius: BorderRadius.circular(1),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: _stripeSpacing),
-            child: Container(
-              color: Colors.purple,
-            ),
-          ),
-        ),
-        SizedBox(height: _spacing),
-        Container(
-          height: 30,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(
-              color: Colors.grey,
-              width: 1,
-            ),
-            borderRadius: BorderRadius.circular(1),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: _stripeSpacing),
-            child: Container(
-              color: Colors.brown,
-            ),
-          ),
-        ),
-        SizedBox(height: _spacing),
-        Container(
-          height: 30,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(
-              color: Colors.grey,
-              width: 1,
-            ),
-            borderRadius: BorderRadius.circular(1),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: _stripeSpacing),
-            child: Container(
-              color: Colors.black,
-            ),
-          ),
-        ),
-        /* FutureBuilder(
-            future: output, //child('techniques').orderByChild('jawara_muda')).equalTo('white').orderByKey().limitToLast(10).onValue
-            builder: (context, snapshot) {
-              final tileList = <ListTile>[];
-              if(!snapshot.hasData || snapshot.hasError){
-                return CircularProgressIndicator();
-              } else {
-                final myOrders = Map<String, dynamic>.from(
-                    (snapshot.data! as DataSnapshot).value);
-                //print(myOrders);
-                myOrders.forEach((key, value){
-                  final nextOrder = Map<String, dynamic>.from(value);
-                  final orderTile = ListTile(
-                    leading: Icon(Icons.local_cafe),
-                    title: Text(nextOrder['desc']),
-                    subtitle: Text(nextOrder['technique'])
-                  );
-                  tileList.add(orderTile);
-                });
-              }
-
-              return Expanded(
-                child: ListView(
-                  children: tileList,
+                  color: Colors.grey,
+                  width: 1,
                 ),
-              );
-            }
+                borderRadius: BorderRadius.circular(_borderRadius),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(width: _stripeSpacing),
+                        Container(
+                          width: _stripeWidth,
+                          color: Colors.black,
+                        ),
+                        SizedBox(width: _stripeSpacing),
+                        Container(
+                          width: _stripeWidth,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          width: _stripeWidth,
+                          color: Colors.black,
+                        ),
+                        SizedBox(width: _stripeSpacing),
+                        Container(
+                          width: _stripeWidth,
+                          color: Colors.black,
+                        ),
+                        SizedBox(width: _stripeSpacing),
+                      ],
+                    ),
+                  ),
+                ],
+              )),
+        );
+      } else {
+        return SizedBox.shrink();
+      }
+    }
+    Future thumbURL = fetchAlbum(dbItem?['vidID']);
+    //print(fetchAlbum(dbItem?['vidID']));
+    //String thumbURL = "https://firebasestorage.googleapis.com/v0/b/silat-moves-app.appspot.com/o/kananKiri.png?alt=media&token=6a710592-a444-4db5-863e-afcf1be3a434";
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        showHeader(),
+        Card(
+          child: ListTile(
+            //leading: Image.network(thumbURL),
+            leading: FutureBuilder(
+            future: fetchAlbum(dbItem?['vidID']),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Image.network(snapshot.data.toString());
+                } else {
+                  return CircularProgressIndicator();
+                }
+              }
+    ),
+            title: Text(dbItem?['technique']),
+            subtitle: Text(dbItem?['desc']),
+            trailing: Icon(Icons.play_circle_outline),
+            onTap: () => {print("Technique Tapped")},
+          ),
         ),
-       */
       ],
     );
   }
-}
-
-/*
-import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
-
-class TechniquesHome extends StatefulWidget {
-  const TechniquesHome({Key? key}) : super(key: key);
-
-  @override
-  _TechniquesHomeState createState() => _TechniquesHomeState();
-}
-
-class _TechniquesHomeState extends State<TechniquesHome> {
-  bool _active = false;
-
-  void _handleTapboxChanged(bool newValue) {
-    setState(() {
-      _active = newValue;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Container(
-        child: Column(
-          children: [
-            Icon(
-              Icons.sports_kabaddi,
-              size: 150,
-            ),
-            Text(
-              "This is the new technique section of the website!"
-            )
-          ],
-        ),
-      ) ,
-    );
-  }
-}
-
-
-
-@override
-  Widget build(BuildContext context) {
-    printFirebase();
     return Scaffold(
       appBar: AppBar(
-        title: Text("Firebase Demo"),
+        title: Text('My Techniques'),
       ),
-      body: FutureBuilder(
-          future: _future,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Text(snapshot.error.toString());
-            } else {
-              return Container(
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 250.0),
-                    Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: TextField(
-                        controller: textcontroller,
-                      ),
-                    ),
-                    SizedBox(height: 30.0),
-                    Center(
-                        child: RaisedButton(
-                            color: Colors.pinkAccent,
-                            child: Text("Save to Database"),
-                            onPressed: () {
-                              //addData(textcontroller.text);
-                              //call method flutter upload
-                            }
-                        )
-                    ),
-                  ],
-                ),
-              );
-            }
-          }
+      body: SafeArea(
+        child: FirebaseAnimatedList(
+          query: FirebaseDatabase.instance
+              .reference()
+              .child('techniques')
+              .orderByChild('jawara_muda')
+              .equalTo('yellow'),
+          itemBuilder: (BuildContext context, DataSnapshot snapshot,
+              Animation<double> animation, int index) {
+            Map dbItemValue = snapshot.value;
+            dbItemValue['key'] = snapshot.key;
+            return _buildTechniqueItem(dbItem: dbItemValue, myIndex: index);
+          },
+        ),
       ),
     );
   }
+}
 
 
+class Album {
+  final int userId;
+  final int id;
+  final String title;
 
-*/
+  Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
